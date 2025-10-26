5727739a258e68991045c5d0c1d57f66e32ca5d1
hi2242 <doodukly1999@gmail.com>
2025-10-26 18:39:49 +0900

    feat: GameController 기본 구조 생성

---
+++ src/controller/gameController.js
@@ -0,0 +1,22 @@
+class GameController {
+    #game;
+
+    /**
+     * 게임 전체를 제어할 메인 메서드
+     */
+    async play() {
+
+        // TODO 1: View를 통해 사용자로부터 자동차 입력 받기
+
+        // TODO 2: View를 통해 사용자로부터 시도할 횟수 입력 받기
+
+        // TODO 3: 입력받은 데이터를 통해 Model의 Game 인스턴스 생성
+
+        // TODO 4: View를 통해 게임의 시작을 알리는 문구 출력
+
+        // TODO 5: Model의 getWinners()를 통해 최종 우승자 리스트를 저장
+
+        // TODO 6: View를 통해 최종 우승자 정보 출력
+
+    }
+}
