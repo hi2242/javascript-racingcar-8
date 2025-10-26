ee57393ffd5acf6795b368aef40d4b88526d65b9
hi2242 <doodukly1999@gmail.com>
2025-10-26 17:53:15 +0900

    refactor: Car 클래스의 메서드에 대한 설명 작성

--- src/model/Car.js
+++ src/model/Car.js
@@ -2,21 +2,37 @@ class Car {
     #name;
     #position;
 
+    /**
+     * Car의 객체를 생성하는 생성자
+     * @param {string} name
+     */
     constructor(name) {
         this.#name = name;
         this.#position = 0;
     }
 
+    /**
+     * 랜덤으로 뽑힌 값을 받아서 4 이상이면 전진시키는 메서드
+     * @param {number} value
+     */
     move(value) {
         if (value >= 4) {
             this.#position++;
         }
     }
 
+    /**
+     * private인 name 정보를 반환하는 메서드
+     * @return {string} name
+     */
     getName() {
         return this.#name;
     }
 
+    /**
+     * private인 position 정보를 반환하는 메서드
+     * @return {number} position
+     */
     getPosition() {
         return this.#position;
     }
