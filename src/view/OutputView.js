a9bcebddbd5fcaa3bb26349c0be431e10e7f3095
hi2242 <doodukly1999@gmail.com>
2025-10-27 11:18:57 +0900

    fix: OutputView instance 메서드에서 static 메서드로 변경

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -4,7 +4,7 @@ class OutputView {
   /**
    * 게임 시작 시 실행 결과라는 문구를 출력해줄 메서드
    */
-  printGameStart() {
+  static printGameStart() {
     MissionUtils.Console.print("\n실행 결과");
   }
 
@@ -12,7 +12,7 @@ class OutputView {
    * 자동차의 위치를 시각적으로 출력하는 메서드
    * @param {Car[]} carList - 자동차 리스트
    */
-  printCurrentPosition(carList) {
+  static printCurrentPosition(carList) {
     carList.map((car) =>
       MissionUtils.Console.print(`${car.getName()} : ${car.getPosition()}`)
     );
@@ -22,7 +22,7 @@ class OutputView {
    * 우승자를 출력하는 메서드
    * @param {string[]} winnerList - 우승 자동차 이름 리스트
    */
-  printWinners(winnerList) {
+  static printWinners(winnerList) {
     MissionUtils.Console.print(winnerList.join(", "));
   }
 }
