ebae0982f61e3f476eaed1f37063fc6ce0263035
hi2242 <doodukly1999@gmail.com>
2025-10-27 18:00:22 +0900

    feat: 유효성 검증 controller에 적용

--- src/controller/gameController.js
+++ src/controller/gameController.js
@@ -1,28 +1,36 @@
-import InputView from '../view/InputView.js';
-import OutputView from '../view/OutputView.js';
-import Game from '../model/Game.js';
+import InputView from "../view/InputView.js";
+import OutputView from "../view/OutputView.js";
+import Game from "../model/Game.js";
+import Validator from "../utils/Validator.js";
 
 class GameController {
-    #game;
+  #game;
 
-    /**
-     * 게임 전체를 제어할 메인 메서드
-     */
-    async play() {
+  /**
+   * 게임 전체를 제어할 메인 메서드
+   */
+  async play() {
+    try {
+      const carListInput = await InputView.readCarNames();
+      Validator.validateCarList(carListInput);
 
-        const carListInput = await InputView.readCarNames();
-        const roundsInput = await InputView.readRounds();
+      const roundsInput = await InputView.readRounds();
+      Validator.validateRounds(roundsInput);
 
-        this.#game = new Game(carListInput);
+      this.#game = new Game(carListInput);
 
-        OutputView.printGameStart();
+      OutputView.printGameStart();
 
-        this.#game.playGame(roundsInput);
+      this.#game.playGame(roundsInput);
 
-        const winners = this.#game.getWinners();
+      const winners = this.#game.getWinners();
 
-        OutputView.printWinners(winners);
+      OutputView.printWinners(winners);
+    } catch (error) {
+        OutputView.printError(error.message);
+        throw error;
     }
+  }
 }
 
 export default GameController;
