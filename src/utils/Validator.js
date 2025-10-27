4c03de7a8c3abf23691d3e6c2231b4aee9ebec04
hi2242 <doodukly1999@gmail.com>
2025-10-27 18:20:50 +0900

    feat: 유효성 검사 추가

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -5,8 +5,18 @@ const Validator = {
         }
         const names = input.split(',');
 
+        if (names.some(name => name.trim().length == 0)) {
+            throw new Error('[ERROR] 자동차의 이름을 공백으로 설정할 수 없습니다.')
+        }
+
         if (names.some(name => name.length > 5)) {
             throw new Error('[ERROR] 긴 이름의 자동차는 허용되지 않습니다.');
+        }
+
+        const nameSet = new Set(names);
+
+        if (nameSet.size !== names.length) {
+            throw new Error('[ERROR] 자동차의 이름은 중복으로 설정할 수 없습니다.');
         }
     },
 
