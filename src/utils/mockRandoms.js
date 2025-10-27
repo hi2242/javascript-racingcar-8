d221654847ba62b214ffbe29e1a55a06eae17dd4
hi2242 <doodukly1999@gmail.com>
2025-10-27 20:12:54 +0900

    feat: 테스트 시 사용할 모킹 함수 파일 생성

---
+++ src/utils/mockRandoms.js
@@ -0,0 +1,9 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+export const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
