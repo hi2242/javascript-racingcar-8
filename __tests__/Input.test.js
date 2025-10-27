c99b43813303360585d85569036a0e821bfffe36
hi2242 <doodukly1999@gmail.com>
2025-10-27 20:26:56 +0900

    feat: InputView 테스트 파일 생성

---
+++ __tests__/Input.test.js
@@ -0,0 +1,29 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import InputView from '../src/view/InputView.js';
+
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+
+describe('InputView 클래스 테스트', () => {
+  test('readCarNames: 자동차 이름을 입력받는다.', async () => {
+    mockQuestions(['pobi,woni,jun']);
+
+    const result = await InputView.readCarNames();
+
+    expect(result).toBe('pobi,woni,jun');
+  });
+
+  test('readRounds: 시도할 횟수를 입력받는다.', async () => {
+    mockQuestions(['5']);
+
+    const result = await InputView.readRounds();
+
+    expect(result).toBe('5');
+  });
+});
